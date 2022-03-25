fe30a6af7a89cebd4c3d8c31a63a91cd68b92df7
khyup <khyup0629@gmail.com>
2022-03-25 05:00:06 +0000

    terraform code modified

--- Ansible/Playbook/ec2-instance/ec2.tf
+++ Ansible/Playbook/ec2-instance/ec2.tf
@@ -14,48 +14,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-data "aws_ami" "amazon" {
-  most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-  owners = ["amazon"]
-}
-
 locals {
   common_tags = {
     "Project" = "hyeob-ansible"
   }
 }
 
-resource "aws_instance" "amazon" {
-  count = 2
-
-  ami           = data.aws_ami.amazon.image_id
-  instance_type = "t2.micro"
-  subnet_id     = local.subnet_groups["public"].ids[0]
-  key_name      = "hyeob-home-keypair"
-
-  associate_public_ip_address = true
-  vpc_security_group_ids = [
-    module.sg__ssh.id,
-  ]
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.vpc.name}-amazon-linux-2"
-    }
-  )
-}
 
 resource "aws_instance" "ubuntu" {
   count = 2
