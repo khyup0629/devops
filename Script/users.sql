77615bdf5996b8207862bad3856f1388c8180f93
khyup <khyup0629@gmail.com>
2022-04-27 06:18:45 +0000

    220427

---
+++ Script/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (cname,name,address,city,,zip) VLAUES ('rich',' Richard Blum',' address1',' Goyang',' 60000');
+INSERT INTO users (cname,name,address,city,,zip) VLAUES ('christine',' Christine Bresnahan',' address2',' Seongnam',' 60001');
+INSERT INTO users (cname,name,address,city,,zip) VLAUES ('barbara',' Barbara Blum',' address3',' Seoul',' 60002');
+INSERT INTO users (cname,name,address,city,,zip) VLAUES ('tim',' Timothy Bresnahan',' address4',' Guri',' 60003');
+INSERT INTO users (cname,name,address,city,,zip) VALUES ('rich',' Richard Blum',' address1',' Goyang',' 60000');
+INSERT INTO users (cname,name,address,city,,zip) VALUES ('christine',' Christine Bresnahan',' address2',' Seongnam',' 60001');
+INSERT INTO users (cname,name,address,city,,zip) VALUES ('barbara',' Barbara Blum',' address3',' Seoul',' 60002');
+INSERT INTO users (cname,name,address,city,,zip) VALUES ('tim',' Timothy Bresnahan',' address4',' Guri',' 60003');
